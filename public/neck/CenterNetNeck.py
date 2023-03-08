e2126e65a658e30980b57e7a2574627e19bb77fa
aoji <aoji@360.cn>
2023-03-08 18:35:00 +0800

    add DWConv and BaseConv

--- public/neck/CenterNetNeck.py
+++ public/neck/CenterNetNeck.py
@@ -7,15 +7,45 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(
-        os.path.abspath(__file__))))
+    os.path.abspath(__file__))))
 sys.path.append(BASE_DIR)
 from public.backbone import DeformableConv2d
 
+
+class DWConv(nn.Module):
+    def __init__(self, in_channels, out_channels, ksize, stride=1):
+        super().__init__()
+        self.dconv = BaseConv(in_channels, in_channels, ksize=ksize, stride=stride, groups=in_channels)
+        self.pconv = BaseConv(in_channels, out_channels, ksize=1, stride=1, groups=1)
+
+    def forward(self, x):
+        x = self.dconv(x)
+        x = self.pconv(x)
+        return x
+
+
+class BaseConv(nn.Module):
+    def __init__(self, in_channels, out_channels, ksize, stride, groups=1, bias=False):
+        super().__init__()
+        pad = (ksize - 1) // 2
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=ksize, stride=stride, padding=pad, groups=groups,
+                              bias=bias)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        return x
+
+
 class CenterNetNeck(nn.Module):
     def __init__(self,
-                 inplanes:list,
-                 num_layers:int=3,
-                 out_channels:list=[256, 128, 64],
+                 inplanes: list,
+                 num_layers: int = 3,
+                 out_channels: list = [256, 128, 64],
+                 upsample=False,
                  depthwise=False):
         """
         @description  :normal CenterNet Neck
@@ -29,59 +59,30 @@ class CenterNetNeck(nn.Module):
         one featuremap with 8x upsample ratio to the input, channel_num = out_channels[-1]
         -------
         """
-        
-        
+
         super(CenterNetNeck, self).__init__()
+
+        Conv = DWConv if depthwise else BaseConv
         self.inplanes = inplanes[-1]
         layers = []
         for i in range(num_layers):
-            if depthwise:
-                layers.append(
-                    nn.Conv2d(in_channels=self.inplanes,
-                        out_channels=self.inplanes,
-                        kernel_size=(3, 3),
-                        stride=1,
-                        padding=1,
-                        groups=self.inplanes,
-                        bias=False),
-                )
-                layers.append(nn.BatchNorm2d(self.inplanes))
-                layers.append(nn.ReLU(inplace=True))
-                layers.append(nn.Conv2d(self.inplanes, out_channels[i], kernel_size=1, stride=1))
+            layers.append(Conv(self.inplanes, out_channels[i], 3, 1))
+
+            if upsample:
+                layers.append(nn.Upsample(scale_factor=2, mode='bilinear'))
             else:
                 layers.append(
-#                     DCN(in_channels=self.inplanes,
-#                         out_channels=out_channels[i],
-#                         kernel_size=(3, 3),
-#                         stride=1,
-#                         padding=1,
-#                         dilation=1,
-#                         deformable_groups=1))
-#                     DeformableConv2d(in_channels=self.inplanes,
-#                                     out_channels=out_channels[i],
-#                                     kernel_size=(3, 3),
-#                                     stride=1,
-#                                     padding=1,
-#                                     groups=1,
-#                                     bias=False)
-                    nn.Conv2d(in_channels=self.inplanes,
-                        out_channels=out_channels[i],
-                        kernel_size=(3, 3),
-                        stride=1,
-                        padding=1)
-                    )
-            layers.append(nn.BatchNorm2d(out_channels[i]))
-            layers.append(nn.ReLU(inplace=True))
-            layers.append(
-                nn.ConvTranspose2d(in_channels=out_channels[i],
-                                   out_channels=out_channels[i],
-                                   kernel_size=4,
-                                   stride=2,
-                                   padding=1,
-                                   output_padding=0,
-                                   bias=False))
-            layers.append(nn.BatchNorm2d(out_channels[i]))
-            layers.append(nn.ReLU(inplace=True))
+                    nn.ConvTranspose2d(in_channels=out_channels[i],
+                                       out_channels=out_channels[i],
+                                       kernel_size=4,
+                                       stride=2,
+                                       padding=1,
+                                       output_padding=0,
+                                       bias=False)
+                )
+                layers.append(nn.BatchNorm2d(out_channels[i]))
+                layers.append(nn.ReLU(inplace=True))
+
             self.inplanes = out_channels[i]
 
         self.public_deconv_head = nn.Sequential(*layers)
@@ -94,7 +95,7 @@ class CenterNetNeck(nn.Module):
                 for i in range(w.size(2)):
                     for j in range(w.size(3)):
                         w[0, 0, i, j] = (1 - math.fabs(i / f - c)) * (
-                            1 - math.fabs(j / f - c))
+                                1 - math.fabs(j / f - c))
                 for c in range(1, w.size(0)):
                     w[c, 0, :, :] = w[0, 0, :, :]
 
